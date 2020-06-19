aa2efc6ef1602e2d2bb54628f93a1dc62188e2ec
YannGendreau <yanngendreau@gmail.com>
2020-06-19 11:10:06 +0200

    AJOUTER la création de produit Crée un formulaire d'ajout entité product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Form\ProductFormType;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class ProductController extends AbstractController
 {
@@ -24,9 +28,30 @@ class ProductController extends AbstractController
     /**
      * @Route("/product/new", name="product_add")
      */
-    public function add()
+    public function add(Request $request, EntityManagerInterface $em)
         {
-            return $this->render('product/add.html.twig');
+            //création du formulaire
+            $productForm = $this->createForm(productFormType::class);
+            //on passe la requete au formulaire
+
+            $productForm->handleRequest($request);
+
+            //on vérifie que le formulaire est envoyé et valide.
+            if ($productForm->isSubmitted() && $productForm->isValid()){
+                //on récupere les données du formulaire
+                $product = $productForm->getData();
+
+                $em->persist($product);
+                $em->flush();
+
+                //redirection sur le liste des produits
+                return $this->redirectToRoute('product_list');
+            }
+
+
+            return $this->render('product/add.html.twig',[
+                'product_form' => $productForm->createView()
+            ]);
 
         }
 
